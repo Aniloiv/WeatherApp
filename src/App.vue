b1e70b514ae8341eebb29aa3bc6cfcbf33993aef
Aniloiv <violinap2013r@gmail.com>
2024-01-13 21:47:52 +0200

    Add components & style & api key for weather app

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<script setup>
+import WeatherSummary from "@/components/WeatherSummary.vue";
+import Highlights from "@/components/Highlights.vue";
+import Coordinates from "@/components/Coordinates.vue";
+import Humidity from "@/components/Humidity.vue";
+import { ref, onMounted } from "vue";
+const WEBHOOK = import.meta.env.VITE_API_KEY;
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+const city = ref("Paris");
+const weatherInfo = ref(null);
+
+function getWeather() {
+  fetch(`${BASE_URL}?q=${city.value}&appid=${WEBHOOK}`)
+    .then((response) => response.json())
+    .then((data) => (weatherInfo.value = data));
+}
+onMounted(getWeather);
+</script>
+
+<template>
+  <div class="page">
+    <main class="main">
+      <div class="container">
+        <div class="laptop">
+          <div class="sections">
+            <section class="section section-left">
+              <div class="info">
+                <div class="city-inner">
+                  <input
+                    type="text"
+                    class="search"
+                    v-model="city"
+                    @keyup.enter="getWeather"
+                  />
+                </div>
+                <WeatherSummary :weatherInfo="weatherInfo" />
+              </div>
+            </section>
+            <section class="section section-right">
+              <Highlights />
+            </section>
+          </div>
+          <div class="sections">
+            <section class="section-bottom">
+              <Coordinates />
+            </section>
+            <section class="section-bottom">
+              <Humidity />
+            </section>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<style lang="sass">
+@import '@/assets/styles/main'
+</style>
---
+++ src/components/WeatherSummary.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="summary">
+    <div
+      style="
+        background-image: url('/src/assets/img/weather-main/thunderstorm.png');
+      "
+      class="pic-main"
+    ></div>
+    <div class="weather">
+      <div class="temp">14 °C</div>
+      <div class="weather-desc text-block">Thunderstorm</div>
+    </div>
+    <div class="city text-block">Paris, FR</div>
+    <div class="date text-block">Thu, March 16, 2023</div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    weatherInfo: Object,
+  },
+};
+</script>
